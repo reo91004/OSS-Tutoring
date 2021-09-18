22ceff278fa5ce99ab3f85fb309e01f7551d83a0
reo91004 <reo91004@gmail.com>
2021-09-18 14:28:44 +0900

    ~Week 03

---
+++ src/Week03/Quiz.java
@@ -0,0 +1,25 @@
+package Week03;
+
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Quiz {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        while (true) {
+            System.out.print("곱하고자 하는 두 수 입력>>");
+            try {
+                int n = sc.nextInt();
+                int m = sc.nextInt();
+                System.out.println(n + "x" + m + "=" + n*m); 
+                break;
+            } catch (InputMismatchException e) {
+                System.out.println("실수는 입력하면 안됩니다.");
+                sc.nextLine();
+            }
+        }
+
+        sc.close();
+    }
+}
